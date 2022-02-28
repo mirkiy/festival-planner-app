c275eec73d8fbab5fe7245cfa979bc4342fdfc2c
mirkiy <mirizachar@gmail.com>
2022-02-28 11:23:58 +0000

    intial structure

---
+++ server/src/test/java/com/codeclan/festivalplannerapp/FestivalPlannerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.festivalplannerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalPlannerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
