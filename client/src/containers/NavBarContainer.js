41276d8f88dfe6ac7038db0ceb28ccf88bef7051
jhtdod <jess.dodge@hotmail.co.uk>
2022-03-01 14:49:22 +0000

    navBar imported

--- client/src/containers/NavBarContainer.js
+++ client/src/containers/NavBarContainer.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText, ClickOutside } from '@trendmicro/react-sidenav';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 
 const NavBarContainer = () => {
+
+    const [expanded, setExpanded] = useState(true);
+
   return (
-    <div>NavBarContainer</div>
+    <ClickOutside
+    onClickOutside={() => {
+        setExpanded(false);
+    }}
+>
+    <SideNav
+        expanded={this.state.expanded}
+        onToggle={(expanded) => {
+            setExpanded(true)}}
+            >
+    
+        <SideNav.Toggle />
+        <SideNav.Nav defaultSelected="home">
+            <NavItem eventKey="home">
+                <NavIcon>
+                    <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+                </NavIcon>
+                <NavText>
+                    Home
+                </NavText>
+            </NavItem>
+        </SideNav.Nav>
+    </SideNav>
+</ClickOutside>
   )
 }
 
