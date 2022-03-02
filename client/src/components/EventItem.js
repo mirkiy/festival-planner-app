48ae0dacb77b9c46bda785276d0dffd049cbeb6b
mirkiy <mirizachar@gmail.com>
2022-03-02 23:31:41 +0000

    basic event list displayed

--- client/src/components/EventItem.js
+++ client/src/components/EventItem.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const EventItem = ({event}) => {
+const EventItem = ({ event }) => {
   return (
-    <li>
-        <h1>{event.title}</h1>
-        <h2>{event.year}</h2>
-    </li>
-  )
-}
+    <div className="event-wrapper">
+      {/* <img
+        src="https://images.api.edinburghfestivalcity.com/18/45/80/184580a0463f6b993a6216672742f20fa1874e61-square-150.jpg"
+        alt="jazz band"
+      /> */}
+      <img src="https://images.api.edinburghfestivalcity.com/18/45/80/184580a0463f6b993a6216672742f20fa1874e61-small-320.jpg" />
 
-export default EventItem
+      <div className="event-wrapper-title-container">
+        <span className="event-wrapper-title">{event.title}</span>
+        {/* <h2>{event.year}</h2> */}
+      </div>
+    </div>
+  );
+};
+
+export default EventItem;
