dcc30a8f34c27534dc45268c515b7bead13d6048
mirkiy <mirizachar@gmail.com>
2022-03-08 16:31:44 +0000

    first work on homepage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <h1>Edinfest planner</h1>
+      <section className="main-page-gallery">
+        <div className="event-wrapper">
+          <img
+            class="rounded shadow"
+            width="320px"
+            height="180px"
+            src="../static/images/art.jpg"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
