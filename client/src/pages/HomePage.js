f3a407475b52fa04dfc76d67acd8586d7f3bf4d2
mirkiy <mirizachar@gmail.com>
2022-03-08 18:31:43 +0000

    images added and displayed

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,40 @@
 import React from "react";
+import ArtImage from "../static/images/art.jpg";
+import BookImage from "../static/images/book.jpg";
+import ImaginationImage from "../static/images/imagination.jpg";
+import JazzImage from "../static/images/jazz.jpg";
+import StorytellingImage from "../static/images/storytelling.jpg";
+import InternationalImage from "../static/images/international.jpg";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
+      <section className="main-page-gallery">
+        <div className="event-wrapper">
+          <img
+            class="rounded shadow"
+            width="320px"
+            height="180px"
+            src={ArtImage}
+          />
+        </div>
+        <div className="event-wrapper">
+          <img
+            class="rounded shadow"
+            width="320px"
+            height="180px"
+            src={BookImage}
+          />
+        </div>
+        <div className="event-wrapper">
+          <img
+            class="rounded shadow"
+            width="320px"
+            height="180px"
+            src={ImaginationImage}
+          />
+        </div>
+      </section>
       <h1>Edinfest planner</h1>
       <section className="main-page-gallery">
         <div className="event-wrapper">
@@ -10,11 +42,27 @@ const HomePage = () => {
             class="rounded shadow"
             width="320px"
             height="180px"
-            src="../static/images/art.jpg"
+            src={JazzImage}
+          />
+        </div>
+        <div className="event-wrapper">
+          <img
+            class="rounded shadow"
+            width="320px"
+            height="180px"
+            src={StorytellingImage}
+          />
+        </div>
+        <div className="event-wrapper">
+          <img
+            class="rounded shadow"
+            width="320px"
+            height="180px"
+            src={InternationalImage}
           />
         </div>
       </section>
-    </div>
+    </>
   );
 };
 
