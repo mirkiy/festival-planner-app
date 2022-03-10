5cf56e128a0a9b1861d8c96c8c06b4dfb8ba1516
clauRamirez <clau.rjimenez@gmail.com>
2022-03-10 00:39:07 +0000

    added stack and text

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,12 +1,66 @@
 import React from "react";
+import Stack from "react-bootstrap/Stack";
 import clauChihuahua from "../static/images/clauChihuahua.jpg";
 import mirkaChihuahua from "../static/images/mirkaChihuahua.jpg";
-import JessieJess from "../static/images/jessiEjess.png"
+import JessieJess from "../static/images/jessiEjess.png";
 
 const AboutPage = () => {
   return (
     <>
       <h2>About Us</h2>
+      <div className="container about-text">
+        <p>
+          Thanks for visiting our final project for the Software Development
+          bootcamp at{" "}
+          <a
+            href="https://codeclan.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CodeClan.{" "}
+          </a>
+        </p>
+        <p>
+          In this web application we've written a Java and Spring Boot server to
+          connect our React client with our MongoDB database and the{" "}
+          <a
+            href="https://api.edinburghfestivalcity.com/explore"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Edinburgh Festivals API
+          </a>
+          .
+        </p>
+        <p>These are the main tools we've used: </p>
+        <Stack direction="horizontal" className="about-icons">
+          <img
+            className="stack-icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
+          />
+          <img
+            className="stack-icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg"
+          />
+          <img
+            className="stack-icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+          />{" "}
+          <img
+            className="stack-icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg"
+          />
+          <img
+            className="stack-icon"
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+          />
+        </Stack>
+        <p style={{ paddingTop: "5vh", marginBottom: "0", fontSize: "1.5rem" }}>
+          <a href="https://github.com/mirkiy/festival-planner-app" target="_blank" rel="noopener noreferrer">
+            Have a peek at our project's repository !
+          </a>
+        </p>
+      </div>
 
       <div class="container about-container">
         {/* Source: https://bootstrapious.com/p/team-page */}
@@ -16,7 +70,6 @@ const AboutPage = () => {
               <img
                 src={clauChihuahua}
                 alt=""
-                // width="100"
                 class="img-fluid rounded-circle mb-3 img-thumbnail shadow"
               />
               <h5 class="mb-0">Clau Ramirez</h5>
@@ -25,12 +78,22 @@ const AboutPage = () => {
               </span>
               <ul class="social mb-0 list-inline mt-3">
                 <li class="list-inline-item">
-                  <a href="#" class="social-link">
+                  <a
+                    href="https://github.com/clauRamirez"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="social-link"
+                  >
                     <i class="fa fa-brands fa-github"></i>
                   </a>
                 </li>
                 <li class="list-inline-item">
-                  <a href="#" class="social-link">
+                  <a
+                    href="https://www.linkedin.com/in/clau-ramirez-6b495a229/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="social-link"
+                  >
                     <i class="fa fa-linkedin"></i>
                   </a>
                 </li>
@@ -52,14 +115,22 @@ const AboutPage = () => {
               </span>
               <ul class="social mb-0 list-inline mt-3">
                 <li class="list-inline-item">
-                  <a href="https://github.com/mirkiy" target="_blank"
-        rel="noopener noreferrer" class="social-link">
+                  <a
+                    href="https://github.com/mirkiy"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="social-link"
+                  >
                     <i class="fa fa-brands fa-github"></i>
                   </a>
                 </li>
                 <li class="list-inline-item">
-                  <a href="https://www.linkedin.com/in/miroslava-zacharova/" target="_blank"
-        rel="noopener noreferrer" class="social-link">
+                  <a
+                    href="https://www.linkedin.com/in/miroslava-zacharova/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="social-link"
+                  >
                     <i class="fa fa-linkedin"></i>
                   </a>
                 </li>
@@ -80,15 +151,23 @@ const AboutPage = () => {
                 Software Developer
               </span>
               <ul class="social mb-0 list-inline mt-3">
-              <li class="list-inline-item">
-                  <a href="https://github.com/jhtdod" target="_blank"
-        rel="noopener noreferrer" class="social-link">
+                <li class="list-inline-item">
+                  <a
+                    href="https://github.com/jhtdod"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="social-link"
+                  >
                     <i class="fa fa-brands fa-github"></i>
                   </a>
                 </li>
                 <li class="list-inline-item">
-                  <a href="https://www.linkedin.com/in/jessie-dodge/" target="_blank"
-        rel="noopener noreferrer" class="social-link">
+                  <a
+                    href="https://www.linkedin.com/in/jessie-dodge/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="social-link"
+                  >
                     <i class="fa fa-linkedin"></i>
                   </a>
                 </li>
